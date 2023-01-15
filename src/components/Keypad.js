309314b9d9ab37d8acdcd3e0bab6ad35b13cce7b
Tatiana <Bowmantatiana2@gmail.com>
2023-01-15 07:20:28 -0800

    commit tree 5da994cffe08d44d0472cfae0c3aec8a6008008e

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,15 @@
-// Code Keypad Component Here
+import React from "react"
 
 function Keypad (){
+
+    function handleOnChange() {
+        console.log('Entering password...')
+    }
+
     return (
-        <div></div>
+        <div>
+            <input onChange={handleOnChange} type="password"/>
+        </div>
     )
 }
 
